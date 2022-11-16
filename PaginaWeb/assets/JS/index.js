f737ab469d21d19132f9b221dc85cb8f5c597f97
LucyVillasenor <lucia.auroravp@gmail.com>
2022-11-15 18:12:07 -0600

    Se añadio el enlace entre la pagina de inicio de sesion y la consulta, reajuste en el tamaño del input en la consulta

--- PaginaWeb/assets/JS/index.js
+++ PaginaWeb/assets/JS/index.js
@@ -21,64 +21,14 @@ var validarUsuario = function(){
             if(usuario[i].usuario == inputUsuario  && usuario[i].contraseña == inputContraseña){ 
                 console.log("credenciales correctas");
                 contador= contador + 1;
+                window.location.href = "/consulta.html";
+                
             }
         }
         if(contador===0){//si no hubiera ninguna coincidencia
             console.log("credenciales incorrectas");
+            window.alert("El usuario o la contraseña es incorrecto, vuelve a intenatrlo");
         }
     });   
 }
-
-var getVenta = function(){
-    let tbodyContainer = document.querySelector(".tablecontainer");
-    let totales= document.querySelector(".totales");
-    var inputFolio = document.getElementById("folioVenta").value;
-    console.log(inputFolio);
-
-    //validar el usuario y contraseña ingresados
-    // fetch(urlUsuario+"getAll",{
-    //     method : "get"
-    //   })
-    fetch("/venta.json")
-      .then(response => { 
-        return response.json();
-      })
-      .then(json =>{ 
-        venta = JSON.stringify(json); 
-        venta = JSON.parse(venta); 
-        var contador = 0; 
-        var num = 1;
-        for (let i = 0; i < venta.length; i++) { 
-            if(venta[i].idVenta == inputFolio){
-                addTabla(venta[i],tbodyContainer, num);
-                contador= contador+1;
-                num = num + 1;  
-            }//if
-        }//for
-        totales.innerHTML = 
-                                `<label> Total Productos:</label>
-                                <label></label>
-                                <label>Total de Venta:</label>
-                                <label></label>`;
-        if(contador===0){//si no hubiera ninguna coincidencia
-            console.log("No hay ventas");
-        }
-    });   
-}
-
-//inyecta la tabla
-function addTabla(item,clase,num){
-    const itemHTML = 
-    `   <div class="trenglon">
-        <tr>
-        <td scope="row">"${num}"</td>
-        <td>"${item.producto}"</td>
-        <td>"${item.cantidad}"</td>
-        <td>"${item.precio}"</td>
-        <td>"${item.total}"</td>
-        </tr>
-        </div>
-    `;
-    clase.innerHTML += itemHTML;
-  }
 
